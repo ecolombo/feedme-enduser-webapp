208a1d33483725d289da1d7dfd5d7cd805b2433b
ecolombo <elia.colombo@gmail.com>
2024-01-03 22:43:44 +0100

    corrected apiBaseUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiBaseUrl :'https://ec2-3-71-89-36.eu-central-1.compute.amazonaws.com:9991/feedme-webservice/api'
+    apiBaseUrl :'http://ec2-3-71-89-36.eu-central-1.compute.amazonaws.com:9991/feedme-webservice/api'
 };
